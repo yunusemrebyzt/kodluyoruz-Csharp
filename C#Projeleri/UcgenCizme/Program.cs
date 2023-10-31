a3751de46c3fb0f898f345bba657f6babba43faa
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2023-10-31 20:55:04 +0300

    UcgenCizme(Proje2)

---
+++ C#Projeleri/UcgenCizme/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir deger giriniz: ");
+            int deger = int.Parse(Console.ReadLine());
+            UcgenCiz(deger);
+
+        }
+
+        static void UcgenCiz(int n)
+        {
+         int k=2;
+         int bosluk=0;
+            for(int i=0;i<n;i++)
+            {
+                Console.Write("\n");
+                for(int l=n-1;l>=bosluk;l--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=1;j<k;j++)
+                {
+                     Console.Write("*");
+                }
+                k= k+2;
+                bosluk++;
+            }
+        }
+
+        
+    }
+}
