0dfbf30785dcf5cc4cebbd702337985fad0c7608
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2023-11-01 22:07:31 +0300

    Algoritma(proje4)

---
+++ C#Projeleri/Algoritma/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program{
+
+
+    static void Main()
+    {
+        Console.WriteLine("String bir değer ve silinmesi istediğiniz indexi arada virgül kullanarak giriniz:  ");
+
+        string giris = Console.ReadLine();
+
+        string[] parca = giris.Split(",");
+
+        string metin = parca[0];
+        int index = int.Parse(parca[1]);
+
+        metin = metin.Remove(index,1);
+        Console.WriteLine( );
+        Console.WriteLine($"silinmiş metin güncel hali {metin}");
+
+
+    }
+}
