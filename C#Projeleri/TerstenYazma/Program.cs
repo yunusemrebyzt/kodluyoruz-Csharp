fbca7d2b061e2ff1d5f54203144a76b757ffcf0f
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2023-11-02 19:49:25 +0300

    TerstenYazma(proje5)

---
+++ C#Projeleri/TerstenYazma/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz: ");
+            string metin = Console.ReadLine();
+
+            char[] harfler = metin.ToCharArray();
+
+            Array.Reverse(harfler);
+
+            string tersMetin = new string(harfler);
+
+            Console.WriteLine(tersMetin);
+        }
+    }
+}
